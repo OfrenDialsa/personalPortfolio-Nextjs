e62c4d182bb6fc2dab64cc0787d37e3c2575c958
Ofren Dialsa <153308025+Neeerooo@users.noreply.github.com>
2024-06-10 00:36:18 +0700

    Update HeroSection.tsx

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-timport Link from "next/link";
+import Link from "next/link";
 import React from "react";
 import { MovingBorderBtn } from "@/components/UI/moving-border";
 import Title  from "./Title";
